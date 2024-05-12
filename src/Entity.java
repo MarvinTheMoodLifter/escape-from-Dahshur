c025efb5bbbf29e89dd91f9fa0691369b3561207
RobertoLupuC <ciprianroberto.lupu@studenti.unipd.it>
2024-05-12 12:34:37 +0200

    Update Entity.java
    prima versione, da modificare in base alle scelte di tutti

--- src/Entity.java
+++ src/Entity.java
@@ -1,1 +1,64 @@
+import java.util.*;
+
+public class Entity
+{
+    private String name;
+    private int health;
+    private int power;
+    private List<Item> items; // bisogna prima creare Item per gestirla meglio
+    private int maxWeight;
+    private int score;
+    private int[] currentPosition;
+
+    public Entity(String name, int health, int power, int maxWeight, int startX, int startY)
+    {
+        this.name = name;
+        this.health = health;
+        this.power = power;
+        this.maxWeight = maxWeight;
+        this.items = new ArrayList<Item>(); // dopo lo sviluppiamo
+        this.score = 0;
+        this.currentPosition = new int[]{startX, startY}; // Usando la matrice bidimensionale
+    }
+
+    public String getName() { return name; }
+
+    public int getHealth() { return health; }
+
+    public int getPower() { return power; }
+
+    public List<Item> getItems() { return items; }
+
+    public double getMaxWeight() { return maxWeight; }
+
+    public int getScore() { return score; }
+
+    public int[] getCurrentPosition() { return currentPosition; }
+
+    public void setName(String name) { this.name = name; }
+
+    public void setHealth(int health) { this.health = health; }
+
+    public void setPower(int power) { this.power = power; }
+
+    public void addItem(String item) { items.add(item); }
+
+    public void setMaxWeight(int maxWeight) { this.maxWeight = maxWeight; }
+
+    public void setScore(int score) { this.score = score; }
+
+    public void setCurrentPosition(int x, int y)
+    {
+        this.currentPosition[0] = x;
+        this.currentPosition[1] = y;
+    }
+
+    public void moveNorth(int maxY) { if (currentPosition[1] < maxY - 1) currentPosition[1]++; }
+
+    public void moveSouth(int maxY) { if (currentPosition[1] > 0) currentPosition[1]--; }
+
+    public void moveEast(int maxX) { if (currentPosition[0] < maxX - 1) currentPosition[0]++; }
+
+    public void moveWest(int maxX) { if (currentPosition[0] > 0) currentPosition[0]--; }
+}
 
