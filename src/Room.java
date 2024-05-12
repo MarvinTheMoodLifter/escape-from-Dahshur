c4f4094d792fc287ad4e6a5c5aeac69b8c364a10
RobertoLupuC <ciprianroberto.lupu@studenti.unipd.it>
2024-05-12 22:04:53 +0200

    Update Room.java

--- src/Room.java
+++ src/Room.java
@@ -1,60 +1,79 @@
 import java.util.*;
+
 public class Room {
-    
-//vector dinamico di oggetti generici 
-private boolean roomstat;
-private ArrayList<Items> roomItems = new ArrayList<Items>();
-private HashMap<String, Boolean> walls = new HashMap<String, Boolean>();
-private ArrayList<Entity> roomnpc = new ArrayList<Entity>();
-public Room(){
-    walls.put("up",false);
-    walls.put("right",false);
-    walls.put("down",false);
-    walls.put("left",false);
-    roomstat = true;
+    private boolean roomstat;
+    private ArrayList<Item> roomItems = new ArrayList<Item>();
+    private HashMap<String, Boolean> walls = new HashMap<String, Boolean>();
+    private ArrayList<Entity> roomnpc = new ArrayList<Entity>();
+
+    public Room() {
+        walls.put("up", false);
+        walls.put("right", false);
+        walls.put("down", false);
+        walls.put("left", false);
+        roomstat = true;
+    }
+
+    public void addItem(Item groundItem) {
+        roomItems.add(groundItem);
+    }
+
+    public void deleteItem(Item target) {
+        roomItems.remove(target);
+    }
+
+    public void clearItem() {
+        roomItems.clear();
+    }
+
+    public boolean hasItem() {
+        return !roomItems.isEmpty();
+    }
+
+    public Boolean getWall(String wallpos) {
+        return walls.getOrDefault(wallpos.toLowerCase(), null);
+    }
+
+    public void setWall(String direction, boolean setting) {
+        walls.replace(direction.toLowerCase(), setting);
+    }
+
+    public boolean hasWall() {
+        return walls.containsValue(true);
+    }
+
+    public void listwall() {
+        System.out.println(walls);
+    }
+
+    public boolean hasEntity() {
+        return !roomnpc.isEmpty();
+    }
+
+    public void addEntity(Entity addednpc) {
+        roomnpc.add(addednpc);
+    }
+
+    public void removeEntity(Entity target) {
+        roomnpc.remove(target);
+    }
+
+    public boolean isOpen() {
+        return roomstat;
+    }
+
+    public void setRoomStat(boolean setting) {
+        roomstat = setting;
+    }
+
+    public void printItems()
+    {
+        if (roomItems.isEmpty()) { System.out.println("No items in the room."); }
+        else
+        {
+            System.out.println("Items in the room:");
+            for (Item item : roomItems) { System.out.println("- " + item.getName()); }
+        }
+    }
+
 }
-public void addItem(Items groundItem){
-   roomItems.addElement(groundItem);
-}
-public void deleteItem(Items target){
-    roomItems.remove(target);
-}
-public void clearItem(){
-    roomItems.clear();
-}
-public boolean hasItem(){
-return !roomItems.isEpmty();
-}
-public boolean getWall(String wallpos){
-    if(walls.containsKey(wallpos.toLowerCase())){
-        walls.get(wallpos);
- } 
-}
-public void setWall(String direction,boolean setting){
-    if(walls.containsKey(direction.toLowerCase())){
-        walls.replace(direction.toLowerCase(),setting);
- } 
-}
-public boolean hasWall(){
-   return walls.containsValue(true);
- }
- public boolean listwall(){
-    System.out.println(walls);
- }
- public boolean hasEntity(){
-    return !roomnpc.isEmpty();
-  }
-  public void addEntity(Entity addednpc){
-    roomnpc.add(addednpc);
-  }
-  public void removeEntity(Entity target){
-    roomnpc.remove(target);
-  }
-  public boolean isOpen(){
-    return roomstat;
-  }
-  public void setRoomStat(Boolean setting){
-    roomstat= setting;
-  }
-}
- 
