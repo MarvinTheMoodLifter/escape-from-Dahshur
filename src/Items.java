91782b8154cd316982ff322cb6876e935ad40969
RobertoLupuC <ciprianroberto.lupu@studenti.unipd.it>
2024-05-15 11:18:50 +0200

    Update Items.java

--- src/Items.java
+++ src/Items.java
@@ -1,27 +1,27 @@
-/**
- * Item.java
- * This class represents an item in the game.
- * The item has a name, a score value, a weight, a location and a description.
- **/
-
 public class Item {
-  private String name;
-  private int score;
-  private int weight;
-  private int damage;
+    private String name;
+    private int score;
+    private int weight;
+    private int damage;
 
-  public Item(String name, int score, int weight, int damage) {
-    this.name = name;
-    this.score = score;
-    this.weight = weight;
-    this.damage = damage;
-  }
+    public Item(String name, int score, int weight, int damage) {
+        this.name = name;
+        this.score = score;
+        this.weight = weight;
+        this.damage = damage;
+    }
 
-  public String getName() { return name; }
+    public String getName() { return name; }
+    public int getItemScore() { return score; }
+    public int getWeight() { return weight; }
+    public int getDamage() { return damage; }
 
-  public int getItemScore() { return score; }
-
-  public int getWeight() { return weight; }
-
-  public int getDamage() { return damage; }
+    public void inspectItem()
+    {
+        System.out.println("Name: " + getName());
+        System.out.println("Score: " + getItemScore());
+        System.out.println("Weight: " + getWeight());
+        System.out.println("Damage: " + getDamage());
+        System.out.print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
+    }
 }
