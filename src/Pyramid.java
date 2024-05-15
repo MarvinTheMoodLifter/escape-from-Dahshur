def87c6d5dc6db61a872ef6ee8ceeed2348ab492
acquanaturaleminerale <simone.ferro.1@studenti.unipd.it>
2024-05-15 20:53:18 +0200

    15/05/2024

--- src/Pyramid.java
+++ src/Pyramid.java
@@ -2,16 +2,24 @@ import java.util.*;
 
 public class Pyramid {
     private Room[][] gamemap;
-public Pyramid(){
-    gamemap = new Room[2][2];
-    for(int i=0;i < 3;i++){
-    for(int j=0;j < 3;j++){
-        gamemap[i][j]= new Room();
+    private String pyramidDescription;
+
+    public Pyramid() 
+    {
+        pyramidDescription="";
+        gamemap = new Room[3][3];
+        for (int i = 0; i < gamemap.length; i++) 
+        {
+            for (int j = 0; j < gamemap[i].length; j++) { gamemap[i][j] = new Room(); }
+        }
     }
+
+    public Room getRoom(int posx, int posy) 
+    {
+        if (posx >= 0 && posx < gamemap.length && posy >= 0 && posy < gamemap[posx].length) { return gamemap[posx][posy]; } 
+        else { return null; }
     }
-    
-}
-public Room getRoom(int posx,int posy){
-    return gamemap[posx][posy];
- }
+    public String getPyramidDesc(){return pyramidDescription;}
+    public void setPyramidDesc(String desc){ pyramidDescription=desc;}
+    public void printPyramidDesc(){System.out.println(pyramidDescription) ;}
 }
