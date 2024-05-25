36907939db349f2c9b50f89c17e1dfd159c059a8
RobertoLupuC <ciprianroberto.lupu@studenti.unipd.it>
2024-05-25 12:00:54 +0200

    Update Room.java

--- src/Room.java
+++ src/Room.java
@@ -1,149 +1,165 @@
 import java.util.*;
 
-public class Room {
-    private boolean roomstat;
-    private String roomdescription;
-    private ArrayList<Item> roomItems = new ArrayList<Item>();
-    private HashMap<String, Boolean> walls = new HashMap<String, Boolean>();
-    private ArrayList<NPC> roomnpc = new ArrayList<NPC>();
-    private Boolean isEntrance;
+public class Main_Character extends Entity
+{
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
-    public Room() {
-        walls.put("up", false);
-        walls.put("right", false);
-        walls.put("down", false);
-        walls.put("left", false);
-        roomstat = true;
-        roomdescription = "";
-        isEntrance = false;
+    private Item equippedItem;
+    private boolean hasEquippedItem;
+    private HashMap<String, Item> inventory;
+
+    public Main_Character(String name, int health, int power, int maxWeight, int startX, int startY)
+    {
+        super(name, health, power, maxWeight, startX, startY);
+        this.equippedItem = null;
+        this.hasEquippedItem = false;
+        this.inventory = new HashMap<>();
     }
 
-    public void addItem(Item groundItem) {
-        roomItems.add(groundItem);
-    }
-
-    public void deleteItem(Item target) {
-        roomItems.remove(target);
-    }
-
-    public void clearItem() {
-        roomItems.clear();
-    }
-
-    public boolean hasItem() {
-        return !roomItems.isEmpty();
-    }
-
-    public Boolean getWall(String wallpos) {
-        return walls.getOrDefault(wallpos.toLowerCase(), null);
-    }
-
-    public void setWall(String direction, boolean setting) {
-        walls.replace(direction.toLowerCase(), setting);
-    }
-
-    public boolean hasWall() {
-        return walls.containsValue(true);
-    }
-
-    public void listwall() {
-        System.out.println(walls);
-    }
-
-    public boolean hasEntity() {
-        return !roomnpc.isEmpty();
-    }
-
-    public void addEntity(NPC addednpc) {
-        roomnpc.add(addednpc);
-    }
-
-    public void removeEntity(NPC target) {
-        roomnpc.remove(target);
-    }
-
-    public boolean isOpen() {
-        return roomstat;
-    }
-
-    public void setRoomStat(boolean setting) {
-        roomstat = setting;
-    }
-
-    public void printItems() {
-        if (roomItems.isEmpty()) {
-            System.out.println("No items in the room.");
-        } else {
-            System.out.println("Items in the room:");
-            for (Item item : roomItems) {
-                System.out.println("- " + item.getName());
-            }
-        }
-    }
-
-    public List<Item> getRoomItems() {
-        return roomItems;
-    }
-
-    public void describeEntities() {
-        if (roomnpc.isEmpty()) {
-            System.out.println("There are no characters in the room.");
-        } else {
-            for (Entity entity : roomnpc) {
-                if (entity instanceof NPC) {
-                    ((NPC) entity).describe();
-                } else {
-                    System.out.println(entity.getName() + " is here.");
-                }
-            }
-        }
-    }
-
-    public void talkToNPC(String npcName) {
-        for (Entity entity : roomnpc) {
-            if (entity instanceof NPC && entity.getName().equalsIgnoreCase(npcName)) {
-                System.out.println(npcName + " says: 'Hello, I am the " + npcName + ". " + ((NPC) entity).getDescription() + "'");
+    public void inspectItemByName(Room room, String itemName)
+    {
+        List<Item> items = room.getRoomItems();
+        for (Item item : items)
+        {
+            if (item.getName().equalsIgnoreCase(itemName))
+            {
+                item.inspectItem();
                 return;
             }
         }
-        System.out.println("No NPC named '" + npcName + "' found in the room.");
+        System.out.println("Item '" + itemName + "' not found in the room.");
     }
 
-    public NPC findNPCByName(String name) {
-        for (NPC npc : roomnpc) {
-            if (npc.getName().equalsIgnoreCase(name)) {
-                return npc;
+    public void attack(NPC npc)
+    {
+        int damage = this.getPower();
+        if (equippedItem != null) { damage += equippedItem.getDamage(); }
+        if (npc.isAlive()) { npc.takeDamage(damage); }
+        else { System.out.println(npc.getName() + " is already dead and cannot be attacked."); }
+    }
+
+    public void takeDamage(int damage)
+    {
+        this.setHealth(this.getHealth() - damage);
+        System.out.println(ANSI_PURPLE + this.getName() + " has taken " + damage + " damage and now has " + this.getHealth() + " health left." + ANSI_RESET);
+        if (this.getHealth() <= 0) { die(); }
+    }
+
+    public void equipItem(Item item)
+    {
+        if (!hasEquippedItem)
+        {
+            this.equippedItem = item;
+            this.hasEquippedItem = true;
+            System.out.println(item.getName() + " equipped. Your damage increased by " + item.getDamage());
+        }
+        else
+        {
+            System.out.println("You are already holding an item.");
+        }
+    }
+
+    public boolean isAlive() { return this.getHealth() > 0; }
+
+    private void die() { System.out.println(this.getName() + " has died."); }
+
+    public void takeItem(Item item, Room room)
+    {
+        if (getInvWeight() + item.getWeight() <= getMaxWeight())
+        {
+            inventory.put(item.getName(), item);
+            room.deleteItem(item);
+            System.out.println(item.getName() + " taken.");
+        }
+        else
+        {
+            System.out.println("Your backpack is too heavy.");
+        }
+    }
+
+    public void dropItem(Item item, Room room)
+    {
+        room.addItem(item);
+        inventory.remove(item.getName());
+        System.out.println(item.getName() + " dropped.");
+    }
+
+    public int getInvWeight()
+    {
+        int totalWeight = 0;
+        for (Item item : inventory.values())
+        {
+            totalWeight += item.getWeight();
+        }
+        return totalWeight;
+    }
+
+    public int getInvScore()
+    {
+        int totalScore = 0;
+        for (Item item : inventory.values())
+        {
+            totalScore += item.getItemScore();
+        }
+        return totalScore;
+    }
+
+    public void equipInvItem(String itemName)
+    {
+        Item item = inventory.get(itemName);
+        if (item != null && !hasEquippedItem)
+        {
+            equipItem(item);
+            inventory.remove(item.getName());
+        }
+        else
+        {
+            System.out.println("Item not found in inventory or already holding an item.");
+        }
+    }
+
+    public void setHasEqItem(boolean setting)
+    {
+        hasEquippedItem = setting;
+    }
+
+    public boolean getHasEqItem()
+    {
+        return hasEquippedItem;
+    }
+
+    public void unequipItem(Room room)
+    {
+        if (hasEquippedItem && equippedItem != null)
+        {
+            if (getInvWeight() + equippedItem.getWeight() <= getMaxWeight())
+            {
+                inventory.put(equippedItem.getName(), equippedItem);
+                System.out.println(equippedItem.getName() + " unequipped.");
+                equippedItem = null;
+                hasEquippedItem = false;
+            }
+            else
+            {
+                dropItem(equippedItem, room);
+                equippedItem = null;
+                hasEquippedItem = false;
             }
         }
-        return null;
-    }
-
-    public Item findItemByName(String name) {
-        for (Item item : roomItems) {
-            if (item.getName().equalsIgnoreCase(name)) {
-                return item;
-            }
+        else
+        {
+            System.out.println("No item is equipped.");
         }
-        return null;
     }
 
-    public void setRoomDesc(String desc) {
-        roomdescription = desc;
+    public Item getInvItem(String name)
+    {
+        return inventory.get(name);
     }
 
-    public String getRoomDesc() {
-        return roomdescription;
-    }
-
-    public void printRoomDesc() {
-        System.out.println(roomdescription);
-    }
-
-    public void setEntrance(Boolean newEntrance) {
-        isEntrance = newEntrance;
-    }
-
-    public Boolean getEntrance() {
-        return isEntrance;
+    public Item setInvItem(Item item)
+    {
+        return inventory.replace(item.getName(), item);
     }
 }
