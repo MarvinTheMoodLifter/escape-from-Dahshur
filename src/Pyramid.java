af466cbee7082aa80b4e32b75cf355aa12610ecb
acquanaturaleminerale <simone.ferro.1@studenti.unipd.it>
2024-05-27 20:34:25 +0200

    27/05/2024
    item initialization

--- src/Pyramid.java
+++ src/Pyramid.java
@@ -1,4 +1,3 @@
-package firstinstance;
 import java.util.*;
 
 public class Pyramid {
@@ -36,9 +35,20 @@ public class Pyramid {
     {
         // Aggiunta di oggetti alla stanza per dimostrazione
         Room room = gamemap[0][0];
-        room.addItem(new Item("Sword", 10, 5, 20));
-        room.addItem(new Item("Shield", 15, 10, 5));
-        room.addItem(new Item("Coconut", 5, 1, 0));
+        room.addItem(new Item("Sword", 10, 5, 20,"pointy stick"));
+        room.addItem(new Item("Shield", 15, 10, 5, "defense is the best offense"));
+        room.addItem(new Item("Coconut", 5, 1, 0,"food"));
+        gamemap[2][1].addItem(new Item("ankh", 10, 1, 0,"an ancient amulet of glittering gold, it used to be worshipped as the symbol of life"));
+        gamemap[1][2].addItem(new Item("key", 10, 1, 0,"a large metal key well decorated with incisions all over the handle and shaft"));
+        gamemap[0][1].addItem(new Item("djed", 20, 1, 0,"an ancient amulet, its symbol represents stability"));
+        gamemap[0][1].addItem(new Item("khol", 5, 1, 0,"it kind of looks like a modern eyeliner, it's still used as eye makeup. The small wooden object is carvedin the shape of a diety an fully painted"));
+        gamemap[2][2].addItem(new Item("mhyrr oil", 5, 5, 0,"a small bottle of perfume, it's still full and emanates a pleasant smell"));
+        gamemap[2][2].addItem(new Item("linen tunic", 10, 5, 1,"just an average linen tunic, the only remarkable feature being the quality of its cloth"));
+        gamemap[1][0].addItem(new Item("chisel and hammer", 1, 10, 15,"Hammer and chisel, probably left behind by the last workers that worked on the decorations"));
+        gamemap[1][2].addItem(new Item("razor", 1, 2, 10,"a sharp razor with a copper blade and a bone handle"));
+        gamemap[2][2].addItem(new Item("book of the dead", 20, 2, 5,"an ancient papyrus filled with magical formulas whose original purpose was to aid the deceased on their journey to the afterlife"));
+        gamemap[1][1].addItem(new Item("wine", 10, 10, 5,"a big clay jar filled with wine"));
+        gamemap[1][1].addItem(new Item("meat", 1, 10, 1,"rancid meat still sealed inside a clay jar, the smell is slowly leaking from the sealed container"));
 
         // Aggiunta di un NPC alla stanza
         NPC guardian = new NPC("Osiris", 100, 75, 150, 0, 0, "A creepy, ancient mummy that watches over the tomb.", false);
@@ -68,4 +78,3 @@ public class Pyramid {
         else { System.out.println("No room found at the specified coordinates."); }
     }
 }
-
