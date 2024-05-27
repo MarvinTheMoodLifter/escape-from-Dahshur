664fcc51ee83707849e394c894b39e647e7bf2a9
RobertoLupuC <ciprianroberto.lupu@studenti.unipd.it>
2024-05-27 12:27:28 +0200

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,34 +1,30 @@
-package firstinstance;
-
 public class Item {
-  private String name;
-  private int score;
-  private int weight;
-  private int damage;
-  private String itemDescription;
 
-  public Item(String name, int score, int weight, int damage) {
-      this.name = name;
-      this.score = score;
-      this.weight = weight;
-      this.damage = damage;
-      itemDescription= "";
-  }
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    private String name;
+    private int score;
+    private int weight;
+    private int damage;
 
-  public String getName() { return name; }
-  public int getItemScore() { return score; }
-  public int getWeight() { return weight; }
-  public int getDamage() { return damage; }
-  public String getItemDesc() { return itemDescription; }
-  public void setItemDesc(String desc) { itemDescription=desc; }
-  public void printItemDesc() { System.out.println(itemDescription); }
+    public Item(String name, int score, int weight, int damage) {
+        this.name = name;
+        this.score = score;
+        this.weight = weight;
+        this.damage = damage;
+    }
 
-  public void inspectItem()
-  {
-      System.out.println("Name: " + getName());
-      System.out.println("Score: " + getItemScore());
-      System.out.println("Weight: " + getWeight());
-      System.out.println("Damage: " + getDamage());
-      System.out.print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
-  }
+    public String getName() { return name; }
+    public int getItemScore() { return score; }
+    public int getWeight() { return weight; }
+    public int getDamage() { return damage; }
+
+    public void inspectItem()
+    {
+        System.out.println(ANSI_PURPLE + "Name: " + getName());
+        System.out.println("Score: " + getItemScore());
+        System.out.println("Weight: " + getWeight());
+        System.out.println("Damage: " + getDamage() + ANSI_RESET);
+        System.out.print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
+    }
 }
